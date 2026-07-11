3d9bebcc6c950115557b9a6f23ed754ab91d5b99
lianet <jack.tolmak@yandex.ru>
2026-07-11 19:48:38 +0300

    feat: add diabetes-treatment page

--- src/js/modules/swipers.js
+++ src/js/modules/swipers.js
@@ -1,21 +1,29 @@
-const CARD_SLIDER_SELECTORS = [".directions__slider", ".doctors__slider", ".steps__slider", ".conditions__slider", ".rates__slider", ".reviews__slider"];
+const CARD_SLIDER_SELECTORS = [".directions__slider", ".doctors__slider", ".steps__slider", ".rates__slider", ".reviews__slider"];
+
+function getSliderPaginationConfig(sliderEl) {
+	return {
+		el: sliderEl.querySelector(".swiper__pagination"),
+		type: "fraction",
+		renderFraction: function (currentClass, totalClass) {
+			return '<span class="' + currentClass + '"></span>' + " / " + '<span class="' + totalClass + '"></span>';
+		},
+	};
+}
+
+function getSliderNavigationConfig(sliderEl) {
+	return {
+		nextEl: sliderEl.querySelector(".swiper__next"),
+		prevEl: sliderEl.querySelector(".swiper__prev"),
+	};
+}
 
 function getCardSliderConfig(sliderEl) {
 	return {
 		slidesPerView: "auto",
 		speed: 600,
 		spaceBetween: 16,
-		pagination: {
-			el: sliderEl.querySelector(".swiper__pagination"),
-			type: "fraction",
-			renderFraction: function (currentClass, totalClass) {
-				return '<span class="' + currentClass + '"></span>' + " / " + '<span class="' + totalClass + '"></span>';
-			},
-		},
-		navigation: {
-			nextEl: sliderEl.querySelector(".swiper__next"),
-			prevEl: sliderEl.querySelector(".swiper__prev"),
-		},
+		pagination: getSliderPaginationConfig(sliderEl),
+		navigation: getSliderNavigationConfig(sliderEl),
 		breakpoints: {
 			743.98: {
 				slidesPerView: 2,
@@ -29,12 +37,41 @@ function getCardSliderConfig(sliderEl) {
 	};
 }
 
+function getConditionsSliderConfig(sliderEl) {
+	return {
+		slidesPerView: "auto",
+		speed: 600,
+		spaceBetween: 16,
+		pagination: getSliderPaginationConfig(sliderEl),
+		navigation: getSliderNavigationConfig(sliderEl),
+		breakpoints: {
+			743.98: {
+				slidesPerView: 2,
+				spaceBetween: 24,
+			},
+			1199.98: {
+				slidesPerView: 3,
+				spaceBetween: 24,
+			},
+			1439.98: {
+				slidesPerView: 4,
+				spaceBetween: 24,
+			},
+		},
+	};
+}
+
 function initCardSliders() {
 	CARD_SLIDER_SELECTORS.forEach((selector) => {
-		const sliderEl = document.querySelector(selector);
-		if (!sliderEl) return;
+		document.querySelectorAll(selector).forEach((sliderEl) => {
+			new Swiper(sliderEl, getCardSliderConfig(sliderEl));
+		});
+	});
+}
 
-		new Swiper(sliderEl, getCardSliderConfig(sliderEl));
+function initConditionsSliders() {
+	document.querySelectorAll(".conditions__slider").forEach((sliderEl) => {
+		new Swiper(sliderEl, getConditionsSliderConfig(sliderEl));
 	});
 }
 
@@ -76,5 +113,6 @@ function initGallerySlider() {
 export function initSwipers() {
 	initHeroSlider();
 	initCardSliders();
+	initConditionsSliders();
 	initGallerySlider();
 }

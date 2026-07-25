23a58f9873522039b61f0acac8240d9aff16711f
lianet <jack.tolmak@yandex.ru>
2026-07-25 12:54:43 +0300

    feat: add women-health-18-40 page

--- src/js/modules/slideToggle.js
+++ src/js/modules/slideToggle.js
@@ -1,76 +1,78 @@
 export default function initSlideToggle() {
-    HTMLElement.prototype.slideToggle = function (duration, callback) {
-        if (this.clientHeight === 0) {
-            _s(this, duration, callback, true);
-        } else {
-            _s(this, duration, callback);
-        }
-    };
+	HTMLElement.prototype.slideToggle = function (duration = 400, callback) {
+		if (this.hidden || this.clientHeight === 0) {
+			this.slideDown(duration, callback);
+		} else {
+			this.slideUp(duration, callback);
+		}
+	};
 
-    HTMLElement.prototype.slideUp = function (duration, callback) {
-        _s(this, duration, callback);
-    };
+	HTMLElement.prototype.slideUp = function (duration = 400, callback) {
+		_slide(this, duration, callback, false);
+	};
 
-    HTMLElement.prototype.slideDown = function (duration, callback) {
-        _s(this, duration, callback, true);
-    };
+	HTMLElement.prototype.slideDown = function (duration = 400, callback) {
+		_slide(this, duration, callback, true);
+	};
 
-    function _s(el, duration, callback, isDown) {
-        if (typeof duration === 'undefined') duration = 400;
-        if (typeof isDown === 'undefined') isDown = false;
+	function _finish(el, callback) {
+		el.style.removeProperty("height");
+		el.style.removeProperty("padding-top");
+		el.style.removeProperty("padding-bottom");
+		el.style.removeProperty("margin-top");
+		el.style.removeProperty("margin-bottom");
+		el.style.removeProperty("overflow");
+		el.style.removeProperty("transition-duration");
+		el.style.removeProperty("transition-property");
+		el.classList.remove("_slide");
+		if (typeof callback === "function") callback();
+	}
 
-        el.style.overflow = "hidden";
-        if (isDown) el.style.display = "block";
+	function _slide(el, duration, callback, isDown) {
+		if (el.classList.contains("_slide")) return;
 
-        const elStyles = window.getComputedStyle(el);
+		el.classList.add("_slide");
 
-        const elHeight = parseFloat(elStyles.getPropertyValue('height'));
-        const elPaddingTop = parseFloat(elStyles.getPropertyValue('padding-top'));
-        const elPaddingBottom = parseFloat(elStyles.getPropertyValue('padding-bottom'));
-        const elMarginTop = parseFloat(elStyles.getPropertyValue('margin-top'));
-        const elMarginBottom = parseFloat(elStyles.getPropertyValue('margin-bottom'));
+		if (isDown) {
+			el.hidden = false;
+			el.style.removeProperty("display");
 
-        const stepHeight = elHeight / duration;
-        const stepPaddingTop = elPaddingTop / duration;
-        const stepPaddingBottom = elPaddingBottom / duration;
-        const stepMarginTop = elMarginTop / duration;
-        const stepMarginBottom = elMarginBottom / duration;
+			const height = el.offsetHeight;
 
-        let start;
+			el.style.overflow = "hidden";
+			el.style.height = "0";
+			el.style.paddingTop = "0";
+			el.style.paddingBottom = "0";
+			el.style.marginTop = "0";
+			el.style.marginBottom = "0";
+			el.offsetHeight;
 
-        function step(timestamp) {
-            if (start === undefined) start = timestamp;
+			el.style.transitionProperty = "height, margin, padding";
+			el.style.transitionDuration = `${duration}ms`;
+			el.style.height = `${height}px`;
+			el.style.removeProperty("padding-top");
+			el.style.removeProperty("padding-bottom");
+			el.style.removeProperty("margin-top");
+			el.style.removeProperty("margin-bottom");
 
-            const elapsed = timestamp - start;
+			window.setTimeout(() => _finish(el, callback), duration);
+		} else {
+			el.style.transitionProperty = "height, margin, padding";
+			el.style.transitionDuration = `${duration}ms`;
+			el.style.height = `${el.offsetHeight}px`;
+			el.offsetHeight;
 
-            if (isDown) {
-                el.style.height = `${stepHeight * elapsed}px`;
-                el.style.paddingTop = `${stepPaddingTop * elapsed}px`;
-                el.style.paddingBottom = `${stepPaddingBottom * elapsed}px`;
-                el.style.marginTop = `${stepMarginTop * elapsed}px`;
-                el.style.marginBottom = `${stepMarginBottom * elapsed}px`;
-            } else {
-                el.style.height = `${elHeight - stepHeight * elapsed}px`;
-                el.style.paddingTop = `${elPaddingTop - stepPaddingTop * elapsed}px`;
-                el.style.paddingBottom = `${elPaddingBottom - stepPaddingBottom * elapsed}px`;
-                el.style.marginTop = `${elMarginTop - stepMarginTop * elapsed}px`;
-                el.style.marginBottom = `${elMarginBottom - stepMarginBottom * elapsed}px`;
-            }
+			el.style.overflow = "hidden";
+			el.style.height = "0";
+			el.style.paddingTop = "0";
+			el.style.paddingBottom = "0";
+			el.style.marginTop = "0";
+			el.style.marginBottom = "0";
 
-            if (elapsed >= duration) {
-                el.style.height = "";
-                el.style.paddingTop = "";
-                el.style.paddingBottom = "";
-                el.style.marginTop = "";
-                el.style.marginBottom = "";
-                el.style.overflow = "";
-                if (!isDown) el.style.display = "none";
-                if (typeof callback === "function") callback();
-            } else {
-                window.requestAnimationFrame(step);
-            }
-        }
-
-        window.requestAnimationFrame(step);
-    }
+			window.setTimeout(() => {
+				el.hidden = true;
+				_finish(el, callback);
+			}, duration);
+		}
+	}
 }
